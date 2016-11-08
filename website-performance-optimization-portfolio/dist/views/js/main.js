261428baa590592b7394b91bd17bc1ce306dacf2
U-KANBAN\mmonteiro <mmonteiro@20130417-01.kanban.local>
2016-11-08 16:29:09 -0500

    updated index.html

--- website-performance-optimization-portfolio/dist/views/js/main.js
+++ website-performance-optimization-portfolio/dist/views/js/main.js
@@ -504,7 +504,7 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   var sinValue = Math.sin(document.body.scrollTop / 1250);
   for (var i = 0; i < items.length; i++) {
-    var phase = sinValue + (i % 5));
+    var phase = sinValue + (i % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
