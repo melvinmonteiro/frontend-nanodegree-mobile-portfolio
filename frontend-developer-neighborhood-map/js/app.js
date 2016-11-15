9efc94eccd9a8bca8ad29d4aecb27979498d9730
U-KANBAN\mmonteiro <mmonteiro@20130417-01.kanban.local>
2016-11-15 13:14:08 -0500

    after code review comments

--- frontend-developer-neighborhood-map/js/app.js
+++ frontend-developer-neighborhood-map/js/app.js
@@ -38,7 +38,7 @@
 
         var self = this;
         //for filtering the places
-        self.filter = ko.observable("");
+        self.filter = ko.observable('');
 
         //a array to hold the places.
         self.places = ko.observableArray([]);
@@ -55,12 +55,8 @@
             toggle hide/show 'hide-sidebar' css class
         */
         self.togglePlacesList = function() {
-            if (self.togglePlacesDisplayText() === "Show Place List") {
-                self.togglePlacesDisplayText("Hide Place List");
-            }
-            else {
-                self.togglePlacesDisplayText("Show Place List");
-            }
+            (self.togglePlacesDisplayText() === "Show Place List")
+                ? self.togglePlacesDisplayText("Hide Place List") : self.togglePlacesDisplayText("Show Place List");
             return self.togglePlacesListBoolean(!self.togglePlacesListBoolean());
         };
 
@@ -89,6 +85,12 @@
 
                  //animated the marker
                  place.marker.setAnimation(google.maps.Animation.BOUNCE);
+                 //pan to the marker position
+                 map.panTo(place.marker.getPosition());
+                 //set a little zoom
+                 map.setZoom(14);
+                 //set to center
+                 map.setCenter(place.marker.getPosition());
 
                  //make a ajax call to show related wiki links on click of marker
                  //the set timeout is just used to demonstrate the loading animation based on knockout
@@ -105,39 +107,41 @@
             var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' +place.name + '&format=json&callback=wikiCallback';
             $.ajax({
                      url: wikiUrl,
-                     dataType: "jsonp",
-
-                     success: function ( response ) {
-                         //we empty the wiki links if they were previously loaded
-                         place.relatedWikiLinks([]);
+                     dataType: "jsonp"
+                 })
+                 .done(function(response) {
+                     //we empty the wiki links if they were previously loaded
+                     place.relatedWikiLinks([]);
+                     var infoWindowContent;
+                     if (response[1] !== undefined && response[1].length > 0 ) {
                          for (var i = 0 ; i < response[1].length ; i++ ) {
                              place.relatedWikiLinks.push({title:response[1][i],url:response[3][i]});
                          }
                          //build a html to show on the content info panel
-                         var infoWindowContent = "<h5>"+place.name+"</h5>"+
+                         infoWindowContent = "<h5>"+place.name+"</h5>"+
                                         "<h6>Related Links</h6>"+
                                         "<ul>";
                          place.relatedWikiLinks().forEach(function(wikiLink) {
                             infoWindowContent += "<li> <a href='"+wikiLink.url+"' target='_blank'>"+wikiLink.title+"</a></li>";
                          });
                          infoWindowContent += "</ul>";
-
-                         infowindow.setContent(infoWindowContent);
-                         //disable the animation
-                         place.loadingWikiLinks(false);
-                         //clear the error timeout
-                         clearTimeout(self.wikiRequestTimeout);
-                         //remove the animation
-                         place.marker.setAnimation(null);
                      }
+                     else { //if no related links then show below message
+                         infoWindowContent = "<h5>"+place.name+"</h5>"+
+                                         "<h6>No Related Link found for this museum</h6>"+
+                                         "<ul>";
+                     }
+                     infowindow.setContent(infoWindowContent);
+                     //disable the animation
+                     place.loadingWikiLinks(false);
+                     //remove the animation
+                     place.marker.setAnimation(null);
                  })
-                 .fail(function() {
+                 .fail(function(jqXHR, textStatus) {
                        //console.log("error while making javascript api");
-                       //if failed then clear timeout
-                       clearTimeout(self.wikiRequestTimeout);
                        var errorContainer = document.querySelector('#error-snackbar');
                        var data = {
-                             message: 'Error while making api call to wikipedia.',
+                             message: 'Error while making api call to wikipedia`',
                              timeout: 2000
                            };
                        errorContainer.MaterialSnackbar.showSnackbar(data);
@@ -146,23 +150,7 @@
                        //remove the animation
                        place.marker.setAnimation(null);
                  });
-
-                 //handle if there is a timeout issue.
-                 self.wikiRequestTimeout = setTimeout( function() {
-                         var errorContainer = document.querySelector('#error-snackbar');
-                         var data = {
-                              message: 'Request has timeout to wikipedia api.',
-                              timeout: 2000
-                            };
-                         errorContainer.MaterialSnackbar.showSnackbar(data);
-                         //stop loading animation
-                         place.loadingWikiLinks(false);
-                         //remove the animation
-                         place.marker.setAnimation(null);
-                 } , 8000 );
         };
-
-
 
         //this function is called when the place is clicked in the place list
         self.showInMap = function(place) {
@@ -174,6 +162,12 @@
 
             //animated the marker
             place.marker.setAnimation(google.maps.Animation.BOUNCE);
+            //pan to the marker position
+            map.panTo(place.marker.getPosition());
+            //set a little zoom
+            map.setZoom(14);
+            //set to center
+            map.setCenter(place.marker.getPosition());
 
             //make a ajax call to show related wiki links on click of marker
             //the set timeout is just used to demonstrate the loading animation based on knockout
@@ -183,18 +177,19 @@
 
         };
 
-        /* filtering the place list
-            every time the filter search is typed in this function gets called
-            if the text being searched is included in the search list we should see the place list updated.
-            the function also updates the marker observable, this way the markers are filtered out in the map
-        */
+        /**
+         *filtering the place list
+         *every time the filter search is typed in this function gets called
+         *if the text being searched is included in the search list we should see the place list updated.
+         *the function also updates the marker observable, this way the markers are filtered out in the map
+         */
         self.filteredPlaces = ko.computed(function() {
             var filter = self.filter().toLowerCase();
             if (!filter) {
                self.places().forEach(function(place) {
                     //if filter string is modified and equals nothing we set the marker to show
                     if( place.marker !== undefined)
-                        place.marker.setMap(map);
+                        place.marker.setVisible(true);
                });
                return self.places();
             }
@@ -202,11 +197,11 @@
                  return ko.utils.arrayFilter(self.places(), function(place) {
                         if(place.name.toLowerCase().includes(filter)) {
                             // only those markers will show that match the filter string
-                            place.marker.setMap(map);
+                            place.marker.setVisible(true);
                             return true;
                         }
                         //rest of the markers we hide.
-                        place.marker.setMap(null);
+                        place.marker.setVisible(false);
                         return false;
                  });
             }
@@ -216,10 +211,11 @@
     // initialize the places view model
     var placesViewModel = new PlacesViewModel();
 
-    //map is used to store the map object
-    //this will be called when the map async call loads asynchronously
-    //see this
-    //https://developers.google.com/maps/documentation/javascript/adding-a-google-map
+    /**map is used to store the map object
+     *this will be called when the map async call loads asynchronously
+     *see this
+     *https://developers.google.com/maps/documentation/javascript/adding-a-google-map
+     */
     var map;
     var infowindow;
     function initMap() {
@@ -298,13 +294,11 @@
     //apply knockout bindings
     ko.applyBindings(placesViewModel);
 
-    //Error nandling
-    $(document).ready(function (){
-        if("undefined" == typeof google.maps  || "undefined" == typeof google.maps.places ) {
-            if(typeof(componentHandler) !== 'undefined') {
-                   componentHandler.upgradeAllRegistered();
-            }
-            var errorContainer = document.querySelector('#error-snackbar');
-            errorContainer.MaterialSnackbar.showSnackbar({message: "error loading google api.Check console logs."});
+    //Error handling via onerror handling
+    function mapError(){
+        if(typeof(componentHandler) !== 'undefined') {
+               componentHandler.upgradeAllRegistered();
         }
-    });
+        var errorContainer = document.querySelector('#error-snackbar');
+        errorContainer.MaterialSnackbar.showSnackbar({message: "error loading google api.Check console logs."});
+    }
